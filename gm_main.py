9e4bb4ff914d672eb94cb8f7cda86a61d8e2a587
Manuel Ott <mot@rs1-c724.uibk.ac.at>
2025-10-15 10:12:21 +0200

    replaced atomic number dictionary with ase chemical symbols

--- gm_main.py
+++ gm_main.py
@@ -14,6 +14,7 @@ import subprocess
 from mace_calculators import MACEDipoleCalculator
 from ase.io import read
 from ase.optimize import LBFGS
+from ase.data import atomic_numbers, chemical_symbols
 from abc import ABC, abstractmethod
 from typing import Tuple, Optional, Dict
 import logging
@@ -138,25 +139,6 @@ def test_espaloma_functionality():
     finally:
         print("=" * 60)
         print()
-
-# Periodic numbers
-AtomicNumber = {
-   1: 'H' ,   2: 'He',   3: 'Li',   4: 'Be',   5: 'B' ,   6: 'C' ,   7: 'N' ,   8: 'O' , 
-   9: 'F' ,  10: 'Ne',  11: 'Na',  12: 'Mg',  13: 'Al',  14: 'Si',  15: 'P' ,  16: 'S' , 
-  17: 'Cl',  18: 'Ar',  19: 'K' ,  20: 'Ca',  21: 'Sc',  22: 'Ti',  23: 'V' ,  24: 'Cr', 
-  25: 'Mn',  26: 'Fe',  27: 'Co',  28: 'Ni',  29: 'Cu',  30: 'Zn',  31: 'Ga',  32: 'Ge', 
-  33: 'As',  34: 'Se',  35: 'Br',  36: 'Kr',  37: 'Rb',  38: 'Sr',  39: 'Y' ,  40: 'Zr', 
-  41: 'Nb',  42: 'Mo',  43: 'Tc',  44: 'Ru',  45: 'Rh',  46: 'Pd',  47: 'Ag',  48: 'Cd', 
-  49: 'In',  50: 'Sn',  51: 'Sb',  52: 'Te',  53: 'I' ,  54: 'Xe',  55: 'Cs',  56: 'Ba', 
-  57: 'La',  58: 'Ce',  59: 'Pr',  60: 'Nd',  61: 'Pm',  62: 'Sm',  63: 'Eu',  64: 'Gd', 
-  65: 'Tb',  66: 'Dy',  67: 'Ho',  68: 'Er',  69: 'Tm',  70: 'Yb',  71: 'Lu',  72: 'Hf', 
-  73: 'Ta',  74: 'W' ,  75: 'Re',  76: 'Os',  77: 'Ir',  78: 'Pt',  79: 'Au',  80: 'Hg', 
-  81: 'Tl',  82: 'Pb',  83: 'Bi',  84: 'Po',  85: 'At',  86: 'Rn',  87: 'Fr',  88: 'Ra', 
-  89: 'Ac',  90: 'Th',  91: 'Pa',  92: 'U' ,  93: 'Np',  94: 'Pu',  95: 'Am',  96: 'Cm', 
-  97: 'Bk',  98: 'Cf',  99: 'Es', 100: 'Fm', 101: 'Md', 102: 'No', 103: 'Lr', 104: 'Rf', 
- 105: 'Db', 106: 'Sg', 107: 'Bh', 108: 'Hs', 109: 'Mt', 110: 'Ds', 111: 'Rg', 112: 'Cn', 
- 114: 'Nh', 115: 'Fl', 116: 'Lv', 117: 'Ts', 118: 'Og'}
-
 
 # ============================================================================
 # MODULAR DIPOLE CALCULATION SYSTEM
@@ -500,7 +482,7 @@ def run_next_calculation(mol, msg, calculator, dipole_method='auto', calculate_d
         line_element = line.split()
 
         # convert atomic number to element name
-        atom = AtomicNumber[int(line_element[0])]
+        atom = chemical_symbols[int(line_element[0])]
         atomnames.append(atom)
         
         # convert bohr to Angstroem and store coordinates
@@ -672,13 +654,6 @@ if __name__ == '__main__':
     # ========================================================================
     # DIAGNOSTIC MODE - Run diagnostics if requested
     # ========================================================================
-    
-    if len(sys.argv) > 1 and sys.argv[1] == '--diagnose':
-        diagnose_python_environment()
-        test_espaloma_functionality()
-        print("\nTo run the actual calculation, use:")
-        print("python gm_main.py molecule.xyz")
-        sys.exit(0)
     
     # Configuration options
     DIPOLE_METHOD = 'auto'  # Options: 'auto', 'mace_ml', 'espaloma', 'xtb', 'geometry'
